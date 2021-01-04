9b36d279adc1884562392ec9d19d731122a3c5fa
iezerbaev <i.ezerbaev97@gmail.com>
2021-01-04 19:34:51 +0300

    add version for heroku

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from config import *
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ff27c42a75864f7b443269bde9c3fef9'
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = email_username
-app.config['MAIL_DEFAULT_SENDER'] = email_username
-app.config['MAIL_PASSWORD'] = email_password
+app.config['MAIL_USERNAME'] = 'email_username'
+app.config['MAIL_DEFAULT_SENDER'] = 'email_username'
+app.config['MAIL_PASSWORD'] = 'email_password'
 
 mail = Mail(app)
 db = SQLAlchemy(app)
